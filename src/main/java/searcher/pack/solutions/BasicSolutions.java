a6a687c11b5ba32ae1a2fcec6d8655c266ac1e98
newjade <knewjade@gmail.com>
2017-06-25 01:47:17 +0900

    Improve memory at BasicSolutions

--- src/main/java/searcher/pack/solutions/BasicSolutions.java
+++
@@ -1,8 +0,0 @@
-package searcher.pack.solutions;
-
-import core.column_field.ColumnField;
-import searcher.pack.mino_fields.MinoFields;
-
-public interface BasicSolutions {
-    MinoFields parse(ColumnField columnField);
-}
