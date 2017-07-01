6a202aa1dcf21066cd95e6f4d65528cdbce15126
newjade <knewjade@gmail.com>
2017-07-01 23:00:35 +0900

    Filter 10minos orders

--- src/main/java/_experimental/main/Filter10MinoPerfectMain.java
+++ src/main/java/_experimental/main/Filter10MinoPerfectMain.java
@@ -1,4 +1,4 @@
-package _experimental.unused;
+package _experimental.main;
 
 import common.buildup.BuildUp;
 import common.datastore.OperationWithKey;
@@ -11,12 +11,13 @@ import core.field.FieldFactory;
 import core.mino.Block;
 import core.mino.MinoFactory;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,19 +26,33 @@ import java.util.stream.Stream;
 // 10ミノでパフェできるものをフィルタする
 public class Filter10MinoPerfectMain {
     public static void main(String[] args) throws IOException {
-        String inputFilePath = "output/minosp";
+        // ツモ順ファイル一覧を読み込む
+        String inputFilePath = "deploy/order10each/";
         Path path = Paths.get(inputFilePath);
 
         // ミノの組み合わせを決める
         LinkedList<File> targets = Files.walk(path)
                 .map(Path::toFile)
                 .filter(File::isFile)
+                .filter(file -> !file.isHidden())
                 .collect(Collectors.toCollection(LinkedList::new));
+
+        List<String> successList = new ArrayList<>();
+        List<String> failedList = new ArrayList<>();
+        List<String> skipList = new ArrayList<>();
 
         while (!targets.isEmpty()) {
             // ミノの組みあわせを決定
             File targetFile = targets.pop();
             String name = targetFile.getName().substring(0, 10);
+
+            // パフェ地形のファイルが存在するか
+            File perfectsFile = new File(String.format("deploy/perfect10each/%s.csv", name));
+            if (!perfectsFile.exists()) {
+                System.out.println("skip: no file # " + name);
+                skipList.add(name);
+                continue;
+            }
 
             // ツモ順一覧
             List<LongPieces> pieces = Files.lines(targetFile.toPath(), Charset.forName("UTF-8"))
@@ -45,20 +60,13 @@ public class Filter10MinoPerfectMain {
                     .map(LongPieces::new)
                     .collect(Collectors.toList());
 
-            /// パフェ地形一覧
-            File perfectsFile = new File(String.format("output/pfsp/%s.csv", name));
-            if (!perfectsFile.exists()) {
-                System.out.println("skip: no file # " + name);
-                continue;
-            }
-
+            // パフェ地形一覧
             MinoFactory minoFactory = new MinoFactory();
             List<List<OperationWithKey>> perfects = Files.lines(perfectsFile.toPath(), Charset.forName("UTF-8"))
                     .map(line -> OperationWithKeyInterpreter.parseToList(line, minoFactory))
                     .collect(Collectors.toList());
 
             System.out.println(String.format("%s => %s: %d orders in %d fields", targetFile, name, pieces.size(), perfects.size()));
-
 
             // 最初10ミノをホールドなしでパフェできるか確認
             int height = 4;
@@ -76,65 +84,64 @@ public class Filter10MinoPerfectMain {
                                         .anyMatch(operationWithKeys -> BuildUp.existsValidByOrder(field, operationWithKeys.stream(), forward.toList(), 4, reachableThreadLocal.get())));
                     });
 
-
             // できないパターンがあるときはストップ
             if (noPerfect) {
                 System.out.println("    => skip: no all building");
-                continue;
+                failedList.add(name);
+            } else {
+                System.out.println("    => success");
+                successList.add(name);
             }
-
-            Files.move(targetFile.toPath(), Paths.get(String.format("output/okmino/%s.txt", name)));
         }
 
+        // ファイルに出力
+        String successOutputPath = "output/success.csv";
+        File successOutputFile = new File(successOutputPath);
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(successOutputFile), StandardCharsets.UTF_8))) {
+            successList.stream()
+                    .sorted()
+                    .forEach(line -> {
+                        try {
+                            writer.write(line);
+                            writer.newLine();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+            writer.flush();
+        }
 
-//        String inputFilePath = "output/cut.txt";
-//        Path piecesPath = Paths.get(inputFilePath);
-//        Map<BlockCounter, List<LongPieces>> map = Files.lines(piecesPath, Charset.forName("UTF-8"))
-//                .map(s -> new LongPieces(Filter10MinoPerfectMain.parse(s)))
-//                .collect(Collectors.groupingBy(o -> new BlockCounter(o.getBlockStream())));
-//        System.out.println(map.size());
-//
-//
-//        for (Map.Entry<BlockCounter, List<LongPieces>> entry : map.entrySet()) {
-//            String blocks = entry.getKey().getBlockStream()
-//                    .map(Block::getName)
-//                    .collect(Collectors.joining());
-//            File outputFile = new File(String.format("output/minosp/%s.txt", blocks));
-//            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8))) {
-//                for (Pieces pieces : entry.getValue()) {
-//                    writer.write(pieces.getBlockStream().map(Block::getName).collect(Collectors.joining()));
-//                    writer.newLine();
-//                }
-//                writer.flush();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                System.exit(1);
-//            }
-//        }
-//
-//        HashSet<ReduceLongPieces> collect = Files.lines(piecesPath, Charset.forName("UTF-8"))
-//                .parallel()
-//                .map(s -> new LongPieces(Main.parse(s)).fix())
-//                .collect(Collectors.toCollection(HashSet::new));
-//        System.out.println(collect.size());
-//
+        String failedOutputPath = "output/failed.csv";
+        File failedOutputFile = new File(failedOutputPath);
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(failedOutputFile), StandardCharsets.UTF_8))) {
+            failedList.stream()
+                    .sorted()
+                    .forEach(line -> {
+                        try {
+                            writer.write(line);
+                            writer.newLine();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+            writer.flush();
+        }
 
-
-//        String inputFilePath = "output/sp/TIILLLJJJO.csv";
-//        Path piecesPath = Paths.get(inputFilePath);
-//        Files.lines(piecesPath, Charset.forName("UTF-8"))
-//                .forEach(s -> {
-//                    String[] array = s.split(";");
-//                    Arrays.stream(array)
-//                            .forEach(s1 -> {
-//                                String[] split = s1.split(",");
-//                                if (split[0].equals("T")) {
-//                                    if (split[1].equals("L") || split[1].equals("R")) {
-//                                        System.out.println(s1);
-//                                    }
-//                                }
-//                            });
-//                });
+        String skipOutputPath = "output/skip.csv";
+        File skipOutputFile = new File(skipOutputPath);
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(skipOutputFile), StandardCharsets.UTF_8))) {
+            skipList.stream()
+                    .sorted()
+                    .forEach(line -> {
+                        try {
+                            writer.write(line);
+                            writer.newLine();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+            writer.flush();
+        }
     }
 
     private static Stream<Block> parse(String a) {
