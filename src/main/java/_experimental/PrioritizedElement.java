7cbc0ce92afef92075c94d21c487d8c11fb5e2cf
newjade <knewjade@gmail.com>
2017-10-14 20:35:43 +0900

    Remove experimental

--- src/main/java/_experimental/PrioritizedElement.java
+++
@@ -1,5 +0,0 @@
-package _experimental;
-
-public interface PrioritizedElement {
-    int getPriority();
-}
--- src/main/java/_experimental/TreeVisitor.java
+++
@@ -1,9 +0,0 @@
-package _experimental;
-
-import core.mino.Block;
-
-public interface TreeVisitor {
-    void visit(int depth, Block block);
-
-    boolean execute(int depth);
-}
