ce23f108437cb23b50f67b318423dc9b5d4181a9
knewjade <knewjade@gmail.com>
2022-12-09 21:36:34 +0900

    Remove 180 kicks from default rotation system

--- src/main/java/entry/common/kicks/factory/DefaultMinoRotationFactory.java
+++ src/main/java/entry/common/kicks/factory/DefaultMinoRotationFactory.java
@@ -49,90 +49,10 @@ public class DefaultMinoRotationFactory {
         return blockMap;
     }
 
-    private static EnumMap<Piece, EnumMap<Rotate, Pattern>> createRotate180Map() {
-        EnumMap<Piece, EnumMap<Rotate, Pattern>> blockMap = new EnumMap<>(Piece.class);
-        for (Piece piece : Piece.values()) {
-            EnumMap<Rotate, Pattern> rotateMap = new EnumMap<>(Rotate.class);
-            for (Rotate rotate : Rotate.values()) {
-                Pattern pattern = getPatternRotate180(piece, rotate);
-                rotateMap.put(rotate, pattern);
-            }
-            blockMap.put(piece, rotateMap);
-        }
-        return blockMap;
-    }
-
-    private static Pattern getPatternRotate180(Piece piece, Rotate current) {
-        switch (piece) {
-            case I: {
-                switch (current) {
-                    case Spawn:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {1, -1}, {0, -1}, {-1, -1}, {2, -1}, {3, -1}, {1, -2},
-                        });
-                    case Right:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {-1, -1}, {-1, -2}, {-1, -3}, {-1, 0}, {-1, 1}, {-2, -1},
-                        });
-                    case Reverse:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {-1, 1}, {0, 1}, {1, 1}, {-2, 1}, {-3, 1}, {-1, 2},
-                        });
-                    case Left:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {1, 1}, {1, 0}, {1, -1}, {1, 2}, {1, 3}, {2, 1},
-                        });
-                }
-            }
-            case O: {
-                switch (current) {
-                    case Spawn:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {1, 1},
-                        });
-                    case Right:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {1, -1},
-                        });
-                    case Reverse:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {-1, -1},
-                        });
-                    case Left:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {-1, 1},
-                        });
-                }
-            }
-            default: {
-                switch (current) {
-                    case Spawn:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {0, 0}, {1, 0}, {2, 0}, {1, -1}, {2, -1}, {-1, 0}, {-2, 0}, {-1, -1}, {-2, -1}, {0, 1}, {3, 0}, {-3, 0},
-                        });
-                    case Right:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {0, 0}, {0, -1}, {0, -2}, {-1, -1}, {-1, -2}, {0, 1}, {0, 2}, {-1, 1}, {-1, 2}, {1, 0}, {0, -3}, {0, 3},
-                        });
-                    case Reverse:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {0, 0}, {-1, 0}, {-2, 0}, {-1, 1}, {-2, 1}, {1, 0}, {2, 0}, {1, 1}, {2, 1}, {0, -1}, {-3, 0}, {3, 0},
-                        });
-                    case Left:
-                        return Pattern.noPrivilegeSpins(new int[][]{
-                                {0, 0}, {0, -1}, {0, -2}, {1, -1}, {1, -2}, {0, 1}, {0, 2}, {1, 1}, {1, 2}, {-1, 0}, {0, -3}, {0, 3},
-                        });
-                }
-            }
-        }
-        throw new IllegalStateException();
-    }
-
     private final EnumMap<Piece, EnumMap<Rotate, Pattern>> rightMap = createRightMap();
     private final EnumMap<Piece, EnumMap<Rotate, Pattern>> leftMap = createLeftMap();
-    private final EnumMap<Piece, EnumMap<Rotate, Pattern>> rotate180Map = createRotate180Map();
 
     public MinoRotation create() {
-        return new MinoRotationImpl(rightMap, leftMap, rotate180Map);
+        return new MinoRotationNo180Impl(rightMap, leftMap);
     }
 }
