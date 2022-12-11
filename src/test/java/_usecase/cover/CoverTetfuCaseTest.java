9cc89b5974c46327bbbe76ca3d2112ebb4105143
knewjade <knewjade@gmail.com>
2022-12-11 15:27:21 +0900

    Add a test for reported bugs.

--- src/test/java/_usecase/cover/CoverTetfuCaseTest.java
+++ src/test/java/_usecase/cover/CoverTetfuCaseTest.java
@@ -1156,22 +1156,15 @@ class CoverTetfuCaseTest {
         void case20() throws Exception {
             String fumen = "v115@3gA8GeE8BeA8AeD8DeF8CeG8AeD8JetLJ";
 
-//            {
-//                String command = String.format("cover --tetfu %s --patterns T", fumen);
-//                Log log = RunnerHelper.runnerCatchingLog(() -> EntryPointMain.main(command.split(" ")));
-//
-//                // Log
-//                int all = 1;
-//                assertThat(log.getOutput()).contains(Messages.foundSolutions(0, all, fumen));
-//            }
-//            {
-//                String command = String.format("cover --tetfu %s --patterns T --drop 180 --kicks @jstris180", fumen);
-//                Log log = RunnerHelper.runnerCatchingLog(() -> EntryPointMain.main(command.split(" ")));
-//
-//                // Log
-//                int all = 1;
-//                assertThat(log.getOutput()).contains(Messages.foundSolutions(1, all, fumen));
-//            }
+
+            {
+                String command = String.format("cover --tetfu %s --patterns T --drop 180 --kicks @nullpomino180 --mode tsm", fumen);
+                Log log = RunnerHelper.runnerCatchingLog(() -> EntryPointMain.main(command.split(" ")));
+
+                // Log
+                int all = 1;
+                assertThat(log.getOutput()).contains(Messages.foundSolutions(0, all, fumen));
+            }
             {
                 String command = String.format("cover --tetfu %s --patterns T --drop 180 --kicks @jstris180 --mode tsm", fumen);
                 Log log = RunnerHelper.runnerCatchingLog(() -> EntryPointMain.main(command.split(" ")));
---
+++ theme/README.md
@@ -0,0 +1,1 @@
+The themes here are for testing. The themes for artifacts is at [distribution/theme](/distribution/theme).
